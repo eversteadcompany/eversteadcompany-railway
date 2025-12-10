cf5020e58f1a0e5fefb439db7b108b10f4bdc526
kingsplace022 <kingshooter02@gmail.com>
2025-12-10 15:59:41 +0100

    Fix static/media loading for DEBUG=False production

--- eversteadinvest/urls.py
+++ eversteadinvest/urls.py
@@ -1,4 +1,23 @@
 
+# from django.contrib import admin
+# from django.urls import path, include
+# from django.conf import settings
+# from django.conf.urls.static import static
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', include('home.urls')),  # Include the seedview app's URLs
+#     path('userprofile/', include('userprofile.urls')),
+#     path('investment/', include('investment.urls')),  # Ensure 'investment.urls' is correct
+#     path('connectwallet/', include('connectwallet.urls')),
+# ]
+
+# # Add this at the end to serve media files in development
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -6,13 +25,13 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls')),  # Include the seedview app's URLs
+    path('', include('home.urls')),
     path('userprofile/', include('userprofile.urls')),
-    path('investment/', include('investment.urls')),  # Ensure 'investment.urls' is correct
+    path('investment/', include('investment.urls')),
     path('connectwallet/', include('connectwallet.urls')),
 ]
 
-# Add this at the end to serve media files in development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+# -----------------------------
+# MEDIA FILES (works in DEBUG & PRODUCTION)
+# -----------------------------
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
